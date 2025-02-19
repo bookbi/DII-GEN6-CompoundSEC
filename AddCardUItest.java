77a1097aec1a5c28c22a9402b37ea15f47afbbe3
BOOKBIG <yanapatinkham@gmail.com>
2025-02-19 19:44:23 +0700

    Update 3/2

--- AddCardUItest.java
+++ AddCardUItest.java
@@ -4,7 +4,7 @@ import javax.swing.*;
         import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AddCardUI {
+public class AddCardUItest {
     public static void main(String[] args) {
         // Create the main frame
         JFrame frame = new JFrame("Add New Card");
